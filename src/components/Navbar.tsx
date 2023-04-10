64d47113fe19e4a623b4e422ef841a612d1a0400
SandroXC <sandroxcoelho@gmail.com>
2023-04-10 18:38:41 -0300

    feat:criação Navbar/Pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface NavbarProps {
+  logo: string;
+  links: {
+    title: string;
+    href: string;
+  }[];
+}
+
+const Navbar: React.FC<NavbarProps> = ({ logo, links }) => {
+  return (
+    <nav style={{ backgroundColor: '#0b49f2', display: 'flex', alignItems: 'center', padding: '1rem' }}>
+      <img src={logo} alt="Logo" style={{ marginRight: 'auto' }} />
+      <ul style={{ display: 'flex', listStyle: 'none', margin: 0 }}>
+        {links.map((link) => (
+        <li style={{ marginLeft: '1rem' }} key={link.href}>
+            <Link href={link.href}>
+              {link.title}
+            </Link>
+        </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/Navbar';
 
-const Index: React.FC = () => {
+const Cadastro: React.FC = () => {
   const logo = "/logo.png";
   const links = [
-    { title: "Início", href: "/" },
+    { title: "Index", href: "/" },
     { title: "Carteira", href: "/carteira" },
     { title: "Cadastro", href: "/cadastro" },
     { title: "Suporte", href: "/suporte" },
@@ -14,16 +14,16 @@ const Index: React.FC = () => {
   return (
     <>  
       <Head>
-        <title>Início</title>
+        <title>Cadastro</title>
         <Navbar logo="images/logo.png" links={links} />
       </Head>   
       <main className={styles.main}>     
         <div className={styles.description}>              
-          <h1>Dashboard</h1>
+          <h1>Cadastro</h1>
         </div>  
       </main>  
     </>
   );
 };
 
-export default Index;
+export default Cadastro;
