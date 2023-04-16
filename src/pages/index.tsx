dfaaba073b8ab61e5920d0d03fbfb652f7e4522e
SandroXC <sandroxcoelho@gmail.com>
2023-04-16 12:22:11 -0300

    chore: ajustes code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/Navbar';
-//import Chart from '@/components/Chart';
+import Chart from '@/components/Chart';
 
 const Index: React.FC = () => {
-  const logo = "/logo.svg";
+  const logo = "images/logo.svg";
   const links = [
     { title: "Início", href: "/" },
     { title: "Carteira", href: "/carteira" },
@@ -16,11 +16,14 @@ const Index: React.FC = () => {
     <>  
       <Head>
         <title>Início</title>
-        <Navbar logo="images/logo.svg" links={links} />
+        <Navbar logo={logo} links={links} />
       </Head>   
       <main className={styles.main}>     
         <div className={styles.description}>              
-          <h1>Dashboard</h1>
+          <h1>Dashboard</h1>         
+        </div>
+        <div className={styles.graph}>
+          <Chart/>
         </div>  
       </main>  
     </>
