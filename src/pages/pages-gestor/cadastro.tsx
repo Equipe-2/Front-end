7ef2bc8a756380fcf37df19efd42adb1be867352
SandroXC <sandroxcoelho@gmail.com>
2023-05-05 21:04:14 -0300

    Revert "feat: css"
    This reverts commit cf3f4c9356d9537f89fcccd02ec535dc62ba37d3.

--- src/pages/pages-gestor/cadastro.tsx
+++ src/pages/pages-gestor/cadastro.tsx
@@ -3,12 +3,12 @@ import styles from '@/styles/Home.module.css';
 import Navbar from '@/components/Navbar';
 import Script from 'next/script';
 
-const Cadastros: React.FC = () => {
+const Cadastro: React.FC = () => {
   const logo = 'images/logo.svg';
   const links = [
-    { title: 'Início', href: '/admin' },
-    { title: 'Franqueados', href: '../pages-gestor/franqueados' },
-    { title: 'Cadastros', href: '../pages-gestor/cadastros' },
+    { title: 'Início', href: '/' },
+    { title: 'Carteira', href: '../pages-franch/carteira' },
+    { title: 'Cadastro', href: '../pages-franch/cadastro' },
   ];
 
   return (
@@ -24,27 +24,27 @@ const Cadastros: React.FC = () => {
           <form>
             <div className={styles.campo}>
               <label htmlFor="nome">Nome </label>
-              <input id="nome" className={styles["input-bg"]} type="text" defaultValue="José da Silva" />
+              <input id="nome" type="text" defaultValue="José da Silva" disabled />
             </div>
             <div className={styles.campo}>
               <label htmlFor="email">Email </label>
-              <input id="email" className={styles["input-bg"]} type="email" defaultValue="jose_silva@email.com" required />
+              <input id="email" type="email" required />
             </div>
             <div className={styles.campo}>
               <label htmlFor="cnpj">CNPJ </label>
-              <input id="cnpj" className={styles["input-bg"]} type="text" defaultValue="12.345.678/0001-00" required />
+              <input id="cnpj" type="text" defaultValue="12.345.678/0001-00" disabled />
             </div>
             <div className={styles.campo}>
               <label htmlFor="telefone">Número de telefone </label>
-              <input id="telefone" className={styles["input-bg"]} type="tel" defaultValue="(11) 99999-9999" required />
+              <input id="telefone" type="tel" required />
             </div>
             <div className={styles.campo}>
               <label htmlFor="endereco">Endereço </label>
-              <input id="endereco" className={styles["input-bg"]} type="text" defaultValue="Av. Rudá, 3000" required />
+              <input id="endereco" type="text" required />
             </div>
             <div className={styles.campo}>
               <label htmlFor="cidade">Cidade </label>
-              <input id="cidade" className={styles["input-bg"]} type="text" defaultValue="São Paulo" required />
+              <input id="cidade" type="text" required />
             </div>
             <div className={styles.buttoncontainerform}>
             <button type="submit" className={styles.buttonform}>Atualizar</button> 
@@ -66,4 +66,4 @@ const Cadastros: React.FC = () => {
   );
 };
 
-export default Cadastros;
+export default Cadastro;
